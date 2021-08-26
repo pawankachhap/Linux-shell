ed38f2de52de321f0cffc034f318bea69765fe4b
Pawan Kachhap <pawankkachhap3@gmail.com>
2021-08-26 21:52:08 +0530

    Comparing two numbers

---
+++ hackerrank/script5.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#comparing two numbers
+
+read X
+read Y
+
+if [[ $X > $Y ]]
+then
+    echo "X is greater than Y"
+elif [[ $X < $Y ]]
+then
+    echo "X is less than Y"
+else
+    echo "X is equal to Y"
+fi

ab29ea950ab9938b32ddc4b9a7a9daf8c86c9c56
Pawan Kachhap <pawankkachhap3@gmail.com>
2021-08-26 21:20:27 +0530

    simple calculator

---
+++ hackerrank/script4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#input two numbers X and Y and find their
+#sum, difference, product and quotient
+
+read X
+read Y
+
+echo "$((X+Y))"
+echo "$((X-Y))"
+echo "$((X*Y))"
+echo "$((X/Y))"

4b909a17ac4e6daa715e0179583daf49164f7c91
Pawan Kachhap <pawankkachhap3@gmail.com>
2021-08-27 20:54:04 +0530

    conditionals with triangle

---
+++ hackerrank/script7.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#conditionals with triangle
+
+read X
+read Y
+read Z
+
+if [[ ($X == $Y) && ($X == $Z) ]]
+then
+    echo "EQUILATERAL"
+elif [[ ($X == $Y) || ($X == $Z) || ($Y == $Z) ]]
+then
+    echo "ISOSCELES"
+else
+    echo "SCALENE"
+fi

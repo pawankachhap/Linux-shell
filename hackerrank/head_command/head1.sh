a7b94733c271bf0ae8dd16388b06fdbc8296f6c9
Pawan Kachhap <pawankkachhap3@gmail.com>
2021-09-07 13:19:27 +0530

    adding head command script

---
+++ hackerrank/head_command/head1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Display the first 20 lines of an input file.
+#here the input is provided from STDIN so no need of filename
+
+head -n 20
+
+#If there is a file as argument
+
+# head -n 20 <filename>

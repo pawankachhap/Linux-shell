b5b903c7452bc85639d691be988411a56700dd64
Pawan Kachhap <pawankkachhap3@gmail.com>
2021-09-16 12:10:29 +0530

    adding tail command script

---
+++ hackerrank/head_command/tail1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Display the last 20 lines of an input file.
+#Input is provided from STDIN
+
+tail -n 20
+
+#For a file as argument
+# tail -n 20 <filename>
---
+++ hackerrank/head_command/tail2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Display last 20 characters of an input file
+# Input is provided by STDIN
+
+tail -c 20
+
+# For using a file as argument
+# tail -c 20 <filename>

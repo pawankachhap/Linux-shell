f5035e19b185168742ffd0ab56691b824b8e4f91
Pawan Kachhap <pawankkachhap3@gmail.com>
2021-09-16 12:27:45 +0530

    addint tr command scripts

---
+++ hackerrank/translate/tr1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#In a given fragment of text, replace all parentheses() with box [] brackets
+
+tr "()" "[]"
---
+++ hackerrank/translate/tr2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#In a given fragment of text, delete all the lowercase characters a-z
+
+tr -d "a-z"
